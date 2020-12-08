56febf85c8e91a1a5875e2f70bb7a809fc6a040c
silex189 <antoniojm89@gmail.com>
2020-12-08 20:32:39 +0100

    [MASTER] file_handling and system_commands added

--- file_handling.py
+++ file_handling.py
@@ -3,7 +3,7 @@
 # open() modes
 # 'r', read, default mode, raises an error if file does not exist
 # 'a', appending data, creates the file if does not exist
-# 'w', writing data
+# 'w', overwriting data
 # 'x', create file, raises an error if file exists
 
 # additional open() parameter
@@ -13,7 +13,9 @@
 # open() must to be in a try block and also need to be closed
 try:
     f = open("./demofile.txt")  # 'rt' as a second parameter by default
+    # read() loads all data in a file
     data = f.read()
+    print("Printing all the data in a try block:")
     print(data)
     f.close()
 except IOError:
@@ -23,6 +25,52 @@ except IOError:
 # file must to be specify with an alias
 with open("demofile.txt") as file:
     partial_data = file.read(5)
-    print("printing partial data")
+    print("Printing partial data")
     print(partial_data)
+
+    # readline() allows to load a full line
+    print("printing lines:")
+    print(file.readline())
+    print(file.readline())
+
+
+with open("demofile.txt", "a") as file:
+    print("Appending data to demofile.txt")
+    file.write("\nNow the file has more content.")
+
+
+with open("demofile.txt") as file:
+    print("\nReading appended data: ")
+    print(file.read())
+
+
+with open("demofile.txt", "w") as file:
+    print("overwriting demofile.txt")
+    file.write("This is demofile.txt\nThis file is for testing purposes.\nThank you.")
+
+
+with open("demofile.txt") as file:
+    print("\nPrinting demofile.txt overwritten:")
+    print(file.read())
+
+
+with open("newFile.txt", "x") as file:
+    print("Just creating a new file...")
+
+
+# deleting a file
+import os
+
+# before, checking  if file exists
+if os.path.exists("newFile.txt"):
+    os.remove("newFile.txt")
+else:
+    print("File does not exists.")
+
+
+# deleting a folder
+if os.path.exists("./newFolder"):
+    os.rmdir("./newFolder")
+else:
+    print("Folder does not exists.")
 
---
+++ system_commands.py
@@ -0,0 +1,18 @@
+# os connects Python with System OS
+import os
+
+# system() allows to execute OS commands in a rusty way
+os.system("ls")
+if not os.path.exists("./newFolder"):
+    os.mkdir("newFolder")
+
+if os.path.exists("./newFolder"):
+    os.rmdir("newFolder")
+
+
+# subprocess better executing commands
+import subprocess
+
+subprocess.call(["ls", "-la"])
+
+subprocess.call("date")
